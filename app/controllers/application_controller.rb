588ee6d45ee3eaae60e9bf0055ef1852c8a10dae
hartly97 <mhartwikitree@gmail.com>
2017-07-13 19:03:24 -0500

    signup:sign up, in & out complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-end
+  before_action :authenticate_user!
+
+  protected
+
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      end
+
+    private
+
+    def user_not_authorized
+      flash[:alert] = 'You are not authorized to do that'
+      if current_user
+        redirect_to(request.referrer || root_path)
+      else
+        redirect_to new_user_session_path
+      end
+    end
+  end
