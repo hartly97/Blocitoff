898e0c4e04075000270524cd140f307346432b18
hartly97 <mhartwikitree@gmail.com>
2017-07-26 15:28:41 -0500

    good-one:fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 require 'faker'
 
 3.times do
-  u = User.create!(
+  u = User.new(
     email:    Faker::Internet.safe_email,
-    password: "password"
+    password: "password",
+    name: Faker::Hipster.sentence(1)
   )
   u.skip_confirmation!
   u.save!
@@ -23,6 +24,10 @@ end
 #i.update_attributes(created_at: i.created_at - 3.days)
 
 items = Item.all
+
+items.each do |i|
+  i.update_attributes(created_at: i.created_at - rand(1..7).days)
+end
 
 puts "Seed finished"
 puts "#{User.count} users created"
