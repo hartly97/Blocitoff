94e9bfb01eca917f648c43f3936ff9dd3597ef2f
hartly97 <mhartwikitree@gmail.com>
2017-07-26 15:00:06 -0500

    good-one:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,29 @@
-#require 'faker'
+require 'faker'
 
-#3.times do
-#  u = User.new(
-#    email:    Faker::Internet.safe_email,
-#    password: "password"
-#  )
-#  u.skip_confirmation!
-#  u.save!
-#end
+3.times do
+  u = User.create!(
+    email:    Faker::Internet.safe_email,
+    password: "password"
+  )
+  u.skip_confirmation!
+  u.save!
+end
+
+users = User.all
 
 
-#5.times do
-#  Item.create!(
-#    name:  Faker::Hipster.sentence(3),
-#    user: users.sample
-#  )
-#end
+5.times do
+ Item.create!(
+    name:  Faker::Hipster.sentence(3),
+    user: users.sample
+  )
+end
 
-#items = Item.all
+#i = Item.last #make sure an item is loaded
+#i.update_attributes(created_at: i.created_at - 3.days)
 
-#puts "Seed finished"
-#puts "#{User.count} Users created"
-#puts "#{Item.count} Items created"
+items = Item.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} Items created"
